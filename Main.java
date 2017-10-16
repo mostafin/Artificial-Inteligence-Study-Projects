fed3133babd4f1dc530d8de0205da33bc3fcbdf0
mostafin <ostafm@gmail.com>
2017-10-16 20:47:19 +0200

    Updated

--- Main.java
+++ Main.java
@@ -10,27 +10,26 @@ public class Main {
         int bias=1;
         double wspolczynnik_postepu=0.1;
 
-        //tablice wejściowe
+       //dane wejsciowe
         int[] x1 ={0,1,0,1};
         int[] x2 ={0,0,1,1};
 
         //tabela wynikowa dla AND
         int[] y={0,0,0,1};
 
-        //j-ilość wykonanych pętli na konkretnym zestawie danych
-        //pętla dla pierwszej danej [0,0|0]
+        
         for ( int i = 0,j=0; j < 4; j++ ) {
             perceptron.learn_perceptron( bias, x1[i], x2[i], y[i], wspolczynnik_postepu );
         }
-        //pętla dla drugiej danej [1,0|0]
+        
         for ( int i = 1,j=0; j < 4; j++ ) {
             perceptron.learn_perceptron( bias, x1[i], x2[i], y[i], wspolczynnik_postepu );
         }
-        //pętla dla trzeciej danej [0,1|0]
+        
         for ( int i = 2,j=0; j < 4; j++ ) {
             perceptron.learn_perceptron( bias, x1[i], x2[i], y[i], wspolczynnik_postepu );
         }
-        //pętla dla czwartej danej [1,1|1]
+     
         for ( int i = 1,j=0; j < 4; j++ ) {
             perceptron.learn_perceptron( bias, x1[i], x2[i], y[i], wspolczynnik_postepu );
         }
